579f1a13684dd03e65a55fa6a61c464861a87fce
NiTika1509 <sicnax@gmail.com>
2024-08-13 18:08:03 +0500

    feat: Создан экран приветствия для приложения

---
+++ lib/widgets/app.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(109, 96, 248, 1),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background_home.png'),
+              fit: BoxFit.cover
+          )
+        ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    'to',
+                    style: TextStyle(
+                        fontSize: 45,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white
+                    ),
+                  ),
+                  Text(
+                      'Application',
+                      style: TextStyle(
+                          fontSize: 45,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white
+                      )
+                  ),
+                  SizedBox(height: 150),
+                  _LogInButton()
+                ],
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
+
+
+class _LogInButton extends StatefulWidget {
+  const _LogInButton({super.key});
+
+  @override
+  State<_LogInButton> createState() => _LogInButtonState();
+}
+
+class _LogInButtonState extends State<_LogInButton> {
+
+  void _moveToAuth (){
+    final navigator = Navigator.of(context);
+
+    navigator.pushNamed('/auth');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    const buttonStyle = ButtonStyle(
+      backgroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+      padding: WidgetStatePropertyAll(EdgeInsets.fromLTRB(25, 5, 5, 5)),
+      textStyle: WidgetStatePropertyAll<TextStyle>(
+          TextStyle(
+              color: Color.fromRGBO(109, 96, 248, 1),
+              fontSize: 16,
+              fontWeight: FontWeight.w600
+          )
+      ),
+    );
+
+    return ElevatedButton(
+        style: buttonStyle,
+        onPressed: _moveToAuth,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+                'Log in',
+                style: TextStyle(
+                    color: Color.fromRGBO(109, 96, 248, 1),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                )
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              color: Colors.white,
+              onPressed: () {},
+              style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Color.fromRGBO(109, 96, 248, 1))
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
