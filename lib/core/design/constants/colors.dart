03089868552f9dff33de2253867cf8f0398fb4ee
neekit <sicnax@gmail.com>
2024-10-30 17:52:50 +0500

    refactor: декомпозиция переменных в отдельные файлы для констант

---
+++ lib/core/design/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFFFFFFF);
+const Color subColor = Color(0xEAE9E9FF);
+const Color backgroundColor = Color(0xFF272b33);
+const Color backgroundColor80 = Color(0xCB000000);
+const Color backgroundColor50 = Color(0x80000000);
+const Color foregroundColor = Color(0xFF3c3e44);
+const Color secondaryColor = Color(0xFF4D4D4D);
+const Color starWars = Color(0xFFDECF17);
+const Color enabledBorderColor = Color(0xEAC9C9C9);
+const Color errorBorderColor = Colors.red;
---
+++ lib/core/design/constants/export.dart
@@ -0,0 +1,5 @@
+export 'package:flutter_project/core/design/constants/images.dart';
+export 'package:flutter_project/core/design/constants/colors.dart';
+export 'package:flutter_project/core/design/constants/sized.dart';
+export 'package:flutter_project/core/design/constants/fonts.dart';
+
